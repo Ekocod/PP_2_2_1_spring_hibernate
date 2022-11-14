926465f4f92208edde050e5e420b4334a4086c7d
Eko <ekoslambek@gmail.com>
2022-11-14 11:36:09 +0600

    eko

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,42 @@
 package hiber.dao;
 
 import hiber.model.User;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Transactional
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        Query query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Transactional
+    @Override
+    public List<User> getUserByCar(String model, int series) {
+        Query<User> query = sessionFactory.getCurrentSession()
+                .createQuery("FROM User as u WHERE u.car.model =:model AND u.car.series =:series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series);
+        return query.getResultList();
+    }
 }
